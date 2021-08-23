dcced01ddb4726890913b255dd369e8d8668d424
viet98lx <vietuet98@gmail.com>
2021-08-23 14:15:48 +0700

    add mapping file

--- main.py
+++ main.py
@@ -18,9 +18,20 @@ if __name__ == '__main__':
                         help='file need index')
     parser.add_argument('--index_name', type=str, required=True, default='index_example',
                         help='index name')
+    parser.add_argument('--mapping_field_file', type=str, default=None)
     args = parser.parse_args()
     name = args.index_name
+    mapping_file = args.mapping_field_file
     file_path = args.file
+    if mapping_file is not None:
+        mapping_obj = json.load(mapping_file)
+        response = es.indices.create(
+            index=name,
+            body=mapping_obj,
+            ignore=400  # ignore 400 already exists code
+        )
+        # print out the response:
+        print('response:', response)
     if 'csv' in file_path:
         jsonArray = csv2json(file_path)
         import_csv_elastic(jsonArray, es, name)
